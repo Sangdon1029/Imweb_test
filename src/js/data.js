4ccb062f9d154fdfbd5129d30bb29907e5f9456e
Sangdon1029 <gorist@likelion.org>
2022-05-14 21:04:32 +0900

    :memo: Modal창 구현

---
+++ src/js/data.js
@@ -0,0 +1,54 @@
+const src = "./src/assets/img/item";
+const data = [{
+    index:1,
+    src : `${src}-1.jpeg`,
+    Title: "",
+    content : "",
+},{
+    index:2,
+    src : `${src}-2.jpeg`,
+    Title: "",
+    content : "",
+},{
+    index:3,
+    src : `${src}-3.jpeg`,
+    Title: "",
+    content : "",
+},{
+    index:4,
+    src : `${src}-4.jpeg`,
+    Title: "",
+    content : "",
+},{
+    index:5,
+    src : `${src}-5.jpeg`,
+    Title: "",
+    content : "",
+},{
+    index:6,
+    src : `${src}-6.jpeg`,
+    Title: "",
+    content : "",
+},{
+    index:7,
+    src : `${src}-7.jpeg`,
+    Title: "",
+    content : "",
+},{
+    index:8,
+    src : `${src}-8.jpeg`,
+    Title: "",
+    content : "",
+},{
+    index:9,
+    src : `${src}-9.jpeg`,
+    Title: "",
+    content : "",
+},{
+    index:10,
+    src : `${src}-10.jpeg`,
+    Title: "",
+    content : "",
+}]
+
+export default data;
